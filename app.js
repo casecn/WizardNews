6f9ab06e90493e6ad32bfaeda22362333bd2a0e9
Craig Case <case2cn@gmail.com>
2023-06-27 22:29:54 -0600

    adding express routs

--- app.js
+++ app.js
@@ -1,7 +1,86 @@
 const express = require("express");
+const morgan = require("morgan")
+const postBank = require("./postBank")
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan('dev'));
+app.use(express.static('public'));
+
+
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+
+  const html = `<!DOCTYPE html>
+    <html>
+      <head>
+        <title>Wizard News</title>
+        <link rel="stylesheet" href="/style.css" />
+      </head>
+      <body>
+        <div class"news-list">
+          <header>
+            <img src="/logo.png"/>Wizard News</header>
+            ${posts
+              .map(
+                (post) => `
+              <div class='news-item'>
+                <p>
+                  <span class="news-position">${post.id}. ▲</span>
+                  <a href="/posts/${post.id}">${post.title}</a>
+                  <small>(by ${post.name})</small>
+                </p>
+                <small class="news-info">
+                  ${post.upvotes} upvotes | ${post.date}
+                </small>
+              </div>`
+              )
+              .join("")}
+        </div>
+      </body>
+    </html>
+    `;
+  res.send(html);
+})
+
+app.get('/posts/:id', (req, res, next) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+
+  if(!post.id) {
+    // res.status(404).send("Page not found !")
+    next();
+  }
+  else {
+    const html = `<html>
+        <head>
+          <title>Wizard News</title>
+          <link rel="stylesheet" href="/style.css" />
+        </head>
+        <body>
+          <header>
+            <img src="/logo.png"/>Wizard News</header>
+              <div class='news-item'>
+                <p>
+                  <span class="news-position">${post.id}. ▲</span>
+                  ${post.title}
+                  <small>(by ${post.name})</small>
+                </p>
+                <small class="news-info">
+                  ${post.upvotes} upvotes | ${post.date}
+                </small>
+              </div>
+        </body>
+      </html>
+      `;
+    res.send(html);
+  }
+})
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found on the server</h1>");
+});
+
 
 const PORT = 1337;
 
